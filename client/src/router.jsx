e7eaccab09c634518e6a7828cced7f2c08aff0a2
Daniel Alvaro <danielkisaran@gmail.com>
2025-12-04 19:20:22 +0700

    feat: add router.

---
+++ client/src/router.jsx
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Layout from "./components/templates/Layout";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Books from "./pages/books/Books";
+import Members from "./pages/members/Members";
+import Borrowings from "./pages/borrowings/Borrowings";
+import NewBook from "./pages/books/NewBook";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    id: "root",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      {
+        path: "books",
+        children: [
+          { index: true, element: <Books /> },
+          { path: "create", element: <NewBook /> },
+        ],
+      },
+      {
+        path: "members",
+        children: [{ index: true, element: <Members /> }],
+      },
+      {
+        path: "borrowings",
+        children: [{ index: true, element: <Borrowings /> }],
+      },
+    ],
+  },
+]);
