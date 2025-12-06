b18c670ad82b81268184bd4833c55e70fe3eeb2c
Daniel Alvaro <danielkisaran@gmail.com>
2025-12-06 14:01:56 +0700

    feat: add sidebar menu for authors.

--- client/src/components/molecules/Sidebar.jsx
+++ client/src/components/molecules/Sidebar.jsx
@@ -96,6 +96,35 @@ const Sidebar = ({ children }) => {
             </li>
             <li>
               <NavLink
+                to="/authors"
+                className={({ isActive }) =>
+                  `flex items-center px-2 py-1.5 text-body rounded-base hover:bg-[#f3f7fe] hover:text-cyan group ${
+                    isActive ? "font-bold text-cyan bg-[#f3f7fe]" : ""
+                  }`
+                }
+              >
+                <svg
+                  class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-cyan"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 10V6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v4m3-2 .917 11.923A1 1 0 0 1 17.92 21H6.08a1 1 0 0 1-.997-1.077L6 8h12Z"
+                  />
+                </svg>
+                <span class="flex-1 ms-3 whitespace-nowrap">Authors</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
                 to="/books"
                 className={({ isActive }) =>
                   `flex items-center px-2 py-1.5 text-body rounded-base hover:bg-[#f3f7fe] hover:text-cyan group ${
