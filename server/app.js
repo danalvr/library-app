146946d1d2e1f6a5dd995320d77bb0c5f8227bd7
Daniel Alvaro <danielkisaran@gmail.com>
2025-12-06 15:28:44 +0700

    feat: adjust config server and error handling in directory server.

--- server/app.js
+++ server/app.js
@@ -1,41 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { ZodError } = require("zod");
+
+var indexRouter = require("./routes/index");
+var authorRouter = require("./modules/authors/author.routes");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // Handle Zod validation error
+  if (err instanceof ZodError) {
+    const formattedErrors = {};
+    err.errors.forEach((issue) => {
+      formattedErrors[issue.path[0]] = issue.message;
+    });
+
+    return res.status(400).json({
+      message: "Validation failed",
+      errors: formattedErrors,
+    });
+  }
+
+  // Default error handling
+  const status = err.statusCode || 500;
+  res.status(status).json({
+    message: err.message || "Internal server error",
+  });
 });
 
 module.exports = app;
